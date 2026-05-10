19ec98c4da2d538bb8a7bdfb03dba4423d72125b
venkats56 <149287592+venkats56@users.noreply.github.com>
2026-05-10 12:54:38 +0530

    Azure RG Creation from terraform

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+variable "subscription_id" {}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-githubactions-demo"
+  location = "Central India"
+}
