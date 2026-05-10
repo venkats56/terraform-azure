cb91697b994c70ab2e360640da56fbcc5b870c39
venkats56 <149287592+venkats56@users.noreply.github.com>
2026-05-10 23:13:17 +0530

    added the modules

--- terraform/v1/main.tf
+++ terraform/v1/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
-    backend "azurerm" {
+
+  backend "azurerm" {
     resource_group_name  = "ml2568"
     storage_account_name = "terraformrgazure"
     container_name       = "terraform-state"
@@ -19,12 +20,15 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
+
   resource_provider_registrations = "none"
 }
 
 variable "subscription_id" {}
 
-resource "azurerm_resource_group" "demo" {
-  name     = "rg-githubactions-demo1"
-  location = "Central India"
+module "resource_group" {
+  source = "../modules/resource-group"
+
+  resource_group_name = "rg-githubactions-demo1"
+  location            = "Central India"
 }
