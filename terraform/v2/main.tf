94d9d6e716c4b955a1e2d7f22fd57849f2d2b18a
venkats56 <149287592+venkats56@users.noreply.github.com>
2026-05-10 19:17:49 +0530

    Added the versions

---
+++ terraform/v2/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+    backend "azurerm" {
+    resource_group_name  = "ml2568"
+    storage_account_name = "terraformrgazure"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
+}
+
+variable "subscription_id" {}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-githubactions-demo2"
+  location = "Central India"
+}
