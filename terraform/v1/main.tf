a564b15a5a0a344514e7ab2667141ed1bd32ea3a
venkats56 <149287592+venkats56@users.noreply.github.com>
2026-05-11 09:39:01 +0530

    Updated the code

--- terraform/v1/main.tf
+++ terraform/v1/main.tf
@@ -18,9 +18,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-
   resource_provider_registrations = "none"
 }
 
--- terraform/v2/main.tf
+++ terraform/v2/main.tf
@@ -19,8 +19,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-
   resource_provider_registrations = "none"
 }
 
